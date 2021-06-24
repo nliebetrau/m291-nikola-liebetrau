6e2f8f94bbd1857fdfd9a30ff4cc51d1c9279e25
nliebetrau <60655776+nliebetrau@users.noreply.github.com>
2021-06-24 09:39:24 +0200

    Add files via upload

---
+++ m291-c/Main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue' 
+import App from './App.vue' 
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css' 
+
+createApp(App) 
+  .use(bootstrap) 
+  .mount('#app') 
---
+++ m291-c/components/TodoList.vue
@@ -0,0 +1,22 @@
+<template>
+    <input type="text" size="50" v-model="zielEingabe">
+    <button>Neu</button>
+    <h2>TODO</h2>
+    <p>
+        <input type="radio" name="zielSelect" value="1"> JavaScriptLernen</input>
+    </p>
+    <button>Delete</button>
+</template>
+<script>
+
+export default {
+    name: 'ToDo'
+    props { msg: String },
+    data: function () {
+        return {name,props,data}
+    },
+    methods: {
+        neuziel() {ToDo},
+        deleteZiel() {ToDo}
+    }
+}
